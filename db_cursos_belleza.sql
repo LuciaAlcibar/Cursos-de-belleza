c121e5df9f09cfec881c03d6d4cf90bb37cedd02
milagrosfigueroa <milagrosfigueroa036@gmail.com>
2024-10-18 21:37:40 -0300

    sql actualizado con nuevos cursos y alumnos

--- db_cursos_belleza.sql
+++ db_cursos_belleza.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-10-2024 a las 23:21:07
+-- Tiempo de generación: 19-10-2024 a las 02:31:17
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -45,7 +45,10 @@ INSERT INTO `alumnos` (`ID_alumno`, `nombre`, `apellido`, `dni`, `celular`, `dom
 (2, 'Nancy ', 'Fernandez', 25963852, 2494785212, 'Pinto 714'),
 (3, 'Stella', 'Zubiaurre', 35456985, 2494567154, 'Paz 888'),
 (4, 'Pía', 'Hernandez', 40566396, 2494512087, 'Montiel 471'),
-(5, 'Lucia', 'Garcia', 46444258, 2494635986, 'Av. Colon 1012');
+(5, 'Lucia', 'Garcia', 46444258, 2494635986, 'Av. Colon 1012'),
+(8, 'Delfina ', 'Santillan', 25899741, 2494857694, 'Av. Colon 855'),
+(9, 'Luciano', 'Lopez', 39555963, 2494788963, 'Las Heras 189'),
+(10, 'Matias', 'Muñoz', 29223105, 2494125639, '25 de mayo 87');
 
 -- --------------------------------------------------------
 
@@ -69,9 +72,11 @@ CREATE TABLE `cursos` (
 --
 
 INSERT INTO `cursos` (`ID_curso`, `categoria`, `nombre`, `descripcion`, `duracion`, `profesor`, `costo`, `imagen`) VALUES
-(1, 'maquillaje', 'Maquillaje profesional', 'Curso de automaquilaje profesional', '3 meses', 'Luisa Hernandez', 90000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSws-mllklCQJh0HMrRVVpP7U5qPFIn--8J5w&s'),
-(3, 'peluqueria', 'Peluquería en general', 'Curso de peluquería general y peinados para eventos', '8 meses', 'María Barbieri', 200000, 'https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2024/06/28/17195787567286.jpg'),
-(4, 'cuidados', 'Tratamientos faciales profesionales', 'Curso de cosmetología', '3 meses', 'Analía Etcheberry', 80000, 'https://www.esteticapalermo.com.ar/wp-content/uploads/2021/08/tratamientos-faciales.webp');
+(1, 'maquillaje', 'Maquillaje profesional', 'Curso de automaquilaje profesional: desarrollo de técnicas clave para realzar su belleza natural. Enfoque en la selección de productos, corrección de imperfecciones y creación de looks versátiles, logrando aprender habilidades para un acabado profesi', '3 meses', 'Luisa Hernandez', 90000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSws-mllklCQJh0HMrRVVpP7U5qPFIn--8J5w&s'),
+(3, 'peluqueria', 'Peluquería en general', 'Curso de peluquería general y peinados para eventos: dominación de cortes, peinados y tratamientos capilares. El curso incluye técnicas de coloración y estilizado, permitiendo a los participantes ofrecer un servicio integral y personalizado.', '8 meses', 'María Barbieri', 120000, 'https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2024/06/28/17195787567286.jpg'),
+(4, 'cuidados', 'Tratamientos faciales profesionales', 'Curso de cosmetología: adquirirá conocimientos sobre el cuidado de la piel, técnicas de limpieza facial y tratamientos estéticos. Aprenderá sobre productos cosméticos, su aplicación y personalización, desarrollando habilidades para ofrecer asesoría i', '3 meses', 'Analía Etcheberry', 83000, 'https://www.esteticapalermo.com.ar/wp-content/uploads/2021/08/tratamientos-faciales.webp'),
+(15, 'uñas', 'Graciela Lorea', 'Curso de esmaltado semipermanente: aprenderá técnicas de aplicación y retirada de esmalte semipermanente, así como cuidados y mantenimiento de uñas. Desarrollo de habilidades para crear acabados duraderos y profesionales, asegurando la satisfacción d', '6 meses', 'Analia Fernandez', 75000, 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.elespectador.com%2Fcromos%2Fmoda%2Funas-semipermanentes-estos-5-disenos-de-unas-son-la-mejor-eleccion-para-tus-manos%2F&psig=AOvVaw0x7czInQslzawkSi8rz7gJ&ust=1729383146168000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLjci7OUmYkDFQAAAAAdAAAAABAE'),
+(16, 'Maquillaje', 'Curso de maquillaje profesional para eventos', ' Curso de automaquilaje profesional: dominación de técnicas especializadas para crear looks impactantes y duraderos, adecuados para bodas, fiestas y sesiones fotográficas. Personalización del maquillaje según la ocasión y las características del clie', '3 meses', 'Marianela Gonzalez', 90000, 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fjssfreitesmakeup.com%2Fcomo-dar-un-curso-de-automaquillaje%2F&psig=AOvVaw0EYGbBQ8Bp9cttIiGKGTcO&ust=1729384213935000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPjuxrSYmYkDFQAAAAAdAAAAABAE');
 
 -- --------------------------------------------------------
 
@@ -155,13 +160,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
-  MODIFY `ID_alumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID_alumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `cursos`
 --
 ALTER TABLE `cursos`
-  MODIFY `ID_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `ID_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de la tabla `inscriptos`
